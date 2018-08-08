2ae4da7cb72eed565bee268f977ae28e28ed762f
Cindy Lopez <cindy.lopezoportus@gmail.com>
2018-08-08 11:29:30 -0400

    cambio de api

--- public/src/js/calculate.js
+++ public/src/js/calculate.js
@@ -1,15 +1,15 @@
 btnBalance.addEventListener('click', () => {
   const inputTarjeta = newCardNumber.value;
   const selectTarjeta = selectCard.value;
-  fetch(`https://www.psep.cl/api/Bip.php?&numberBip=${inputTarjeta}`)
+  fetch(`https://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=${inputTarjeta}`)
     .then(response => response.json()
     ).then(data => {
       newCardNumber.value = '';
       console.log(JSON.stringify(data));
       showBalance.innerHTML = `
         <p class="title">Saldo Total</p>
-        <p class="saldoFinal">${data['Saldo  tarjeta']}</p>
-        <p class="idCard">N°${data['N&ordm; tarjeta bip! ']}</p>
+        <p class="saldoFinal">${data['saldoTarjeta']}</p>
+        <p class="idCard">N°${data['id']}</p>
       `;
     });
 });
